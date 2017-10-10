5dc049b9cf1ef029615cab4ef6b3722d29ed3ff8
jimontgomery808 <jimontgomery808@gmail.com>
2017-10-10 11:49:35 -0700

    Added button functionality

--- src/cs245/project/HiyaGUI.java
+++ src/cs245/project/HiyaGUI.java
@@ -19,6 +19,7 @@ public class HiyaGUI extends javax.swing.JFrame {
 
     RandomNameGenerator rng = new RandomNameGenerator();
     private int numberOfGuesses = 0;
+    private int score = 100;
     String answer = null;
 
     
@@ -508,113 +509,326 @@ public class HiyaGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonAMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonAMouseClicked
-    jButtonA.setEnabled(false);        // TODO add your handling code here:
+        jButtonA.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("a")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonAMouseClicked
 
     private void jButtonBMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonBMouseClicked
         jButtonB.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("b")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonBMouseClicked
 
     private void jButtonCMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonCMouseClicked
         jButtonC.setEnabled(false);// TODO add your handling code here:
+        if(isInWord("c")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonCMouseClicked
 
     private void jButtonDMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonDMouseClicked
         jButtonD.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("d")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonDMouseClicked
 
     private void jButtonEMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonEMouseClicked
         jButtonE.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("e")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonEMouseClicked
 
     private void jButtonFMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonFMouseClicked
         jButtonF.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("f")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonFMouseClicked
 
     private void jButtonGMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonGMouseClicked
         jButtonG.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("g")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonGMouseClicked
 
     private void jButtonHMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonHMouseClicked
         jButtonH.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("h")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonHMouseClicked
 
     private void jButtonIMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonIMouseClicked
         jButtonI.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("i")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonIMouseClicked
 
     private void jButtonJMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonJMouseClicked
         jButtonJ.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("j")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonJMouseClicked
 
     private void jButtonKMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonKMouseClicked
         jButtonK.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("k")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonKMouseClicked
 
     private void jButtonLMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonLMouseClicked
         jButtonL.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("l")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonLMouseClicked
 
     private void jButtonMMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonMMouseClicked
         jButtonM.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("m")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonMMouseClicked
 
     private void jButtonNMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonNMouseClicked
         jButtonN.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("n")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonNMouseClicked
 
     private void jButtonOMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonOMouseClicked
         jButtonO.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("o")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonOMouseClicked
 
     private void jButtonPMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonPMouseClicked
         jButtonP.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("p")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonPMouseClicked
 
     private void jButtonQMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonQMouseClicked
         jButtonQ.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("q")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonQMouseClicked
 
     private void jButtonRMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonRMouseClicked
         jButtonR.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("r")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonRMouseClicked
 
     private void jButtonSMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonSMouseClicked
         jButtonS.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("s")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonSMouseClicked
 
     private void jButtonTMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonTMouseClicked
         jButtonT.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("t")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonTMouseClicked
 
     private void jButtonUMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonUMouseClicked
         jButtonU.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("u")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonUMouseClicked
 
     private void jButtonVMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonVMouseClicked
         jButtonV.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("v")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonVMouseClicked
 
     private void jButtonWMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonWMouseClicked
         jButtonW.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("w")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonWMouseClicked
 
     private void jButtonXMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonXMouseClicked
         jButtonX.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("x")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonXMouseClicked
 
     private void jButtonYMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonYMouseClicked
         jButtonY.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("y")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonYMouseClicked
 
     private void jButtonZMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonZMouseClicked
         jButtonZ.setEnabled(false);        // TODO add your handling code here:
+        if(isInWord("z")){
+            
+        }  
+        else{
+            wrongGuess();
+        }
     }//GEN-LAST:event_jButtonZMouseClicked
-    
+
   
     
      
   
+
+    private boolean isInWord(String letter)
+    {
+        return (name.getText().contains(letter));
+    }
+    
+    private void wrongGuess()
+    {
+        if(score != 0)
+        {
+            score -= 10;
+            System.out.print("Score: " + score);
+        }
+        else{
+            System.out.print("You Lose!");
+        }
+    }
+    
+    private void drawAnswer()
+    {
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(HiyaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(HiyaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(HiyaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(HiyaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new HiyaGUI().setVisible(true);
+            }
+        });
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel answerHidden;
