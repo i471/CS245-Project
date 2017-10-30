6d3813cf6ef5bb3f71741dfe3c821a2b84672d4e
jimontgomery808 <jimontgomery808@gmail.com>
2017-10-30 15:14:48 -0700

    changed sudoku cell colors

--- src/cs245/project/Sudoku.java
+++ src/cs245/project/Sudoku.java
@@ -2,9 +2,11 @@
 package cs245.project;
 import java.awt.*;        // Uses AWT's Layout Managers
 import java.awt.event.*;  // Uses AWT's Event Handlers
+import java.text.NumberFormat;
 import java.util.Arrays;
 import javax.swing.*;     // Uses Swing's Container/Components
 import javax.swing.border.Border;
+import javax.swing.text.*;
 
 public class Sudoku extends javax.swing.JFrame 
 {
@@ -13,26 +15,30 @@ public class Sudoku extends javax.swing.JFrame
     private Button quit;
     private int totalScore;
    // Name-constants for the game properties
-   public static final int GRID_SIZE = 9;    // Size of the board
-   public static final int SUBGRID_SIZE = 3; // Size of the sub-grid
+   public static final int BOARD_SIZE = 9;    // Size of the board
+   public static final int SUB_BOARD_SIZE = 3; // Size of the sub-grid
  
    // Name-constants for UI control (sizes, colors and fonts)
    public static final int CELL_SIZE = 60;   // Cell width/height in pixels
    public static final int CANVAS_WIDTH  = 100;
    public static final int CANVAS_HEIGHT = 100;
                                              // Board width/height in pixels
-   public static final Color OPEN_CELL_BGCOLOR = Color.YELLOW;
-   public static final Color OPEN_CELL_TEXT_YES = new Color(0, 255, 0);  // RGB
-   public static final Color OPEN_CELL_TEXT_NO = Color.RED;
-   public static final Color CLOSED_CELL_BGCOLOR = new Color(240, 240, 240); // RGB
-   public static final Color CLOSED_CELL_TEXT = Color.BLACK;
-   public static final Font FONT_NUMBERS = new Font("Monospaced", Font.BOLD, 20);
+   public static final Color INPUT_CELL_COLOR = Color.WHITE;
+   public static final Color INPUT_CELL_TEXT_COLOR = new Color(0, 255, 0);  // RGB
+   public static final Color INPUT_CELL_TEXT_NO = Color.RED;
+   public static final Color FIXED_CELL_COLOR = new Color(240, 240, 240); // RGB
+   public static final Color FIXED_CELL_TEXT = Color.BLACK;
+   public static final Font FONT_NUMBERS = new Font("Arial", Font.BOLD, 20);
  
    // The game board composes of 9x9 JTextFields,
-   // each containing String "1" to "9", or empty String
-   private JTextField[][] tfCells = new JTextField[GRID_SIZE][GRID_SIZE];
+    NumberFormat format = NumberFormat.getIntegerInstance();
+    NumberFormatter formatter = new NumberFormatter(format);
+    
+    
 
-   private int[][] puzzle =
+   //private JTextField[][] tfCells = new JTextField[GRID_SIZE][GRID_SIZE];
+   private JFormattedTextField[][] boardCells;
+   private int[][] solution =
       {{8, 3, 5, 4, 1, 6, 9, 2, 7},
        {2, 9, 6, 8, 5, 7, 4, 3, 1},
        {4, 1, 7, 2, 9, 3, 6, 5, 8},
@@ -42,7 +48,7 @@ public class Sudoku extends javax.swing.JFrame
        {6, 5, 2, 7, 8, 1, 3, 9, 4},
        {9, 8, 1, 3, 4, 5, 2, 7, 6},
        {3, 7, 4, 9, 6, 2, 8, 1, 5}};
-   int[][] tempArray = new int[GRID_SIZE][GRID_SIZE];
+   int[][] tempArray = new int[BOARD_SIZE][BOARD_SIZE];
    // For testing, open only 2 cells.
    private boolean[][] masks =
       {{false, true, true, false, true, false, true, true, false},
@@ -60,6 +66,11 @@ public class Sudoku extends javax.swing.JFrame
     */
    public Sudoku(int score) 
    {
+       formatter.setAllowsInvalid(false);
+       formatter.setMinimum(1);
+       formatter.setMaximum(9);
+       formatter.setOverwriteMode(true);
+       boardCells = new JFormattedTextField[BOARD_SIZE][BOARD_SIZE];
        totalScore = 540 + score;
        escapeKey();
        submit = new Button("submit");
@@ -94,7 +105,7 @@ public class Sudoku extends javax.swing.JFrame
        JPanel panel3 = new JPanel();
        
        
-       panel1.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE )); // 9x9 GridLayout
+       panel1.setLayout(new GridLayout(BOARD_SIZE, BOARD_SIZE )); // 9x9 GridLayout
        panel2.setLayout(new BorderLayout());
        panel3.setLayout(new GridBagLayout());
        panel3.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
@@ -120,30 +131,30 @@ public class Sudoku extends javax.swing.JFrame
        panel3.add(panel2,gbc);
  
       // Construct 9x9 JTextFields and add to the content-pane
-      for (int row = 0; row < GRID_SIZE; ++row) 
+      for (int row = 0; row < BOARD_SIZE; ++row) 
       {
-         for (int col = 0; col < GRID_SIZE; ++col) 
+         for (int col = 0; col < BOARD_SIZE; ++col) 
          {
-            tfCells[row][col] = new JTextField();        // Allocate element of array
-            panel1.add(tfCells[row][col]);            // ContentPane adds JTextField
+            boardCells[row][col] = new JFormattedTextField(formatter); // Allocate element of array
+            panel1.add(boardCells[row][col]);            // ContentPane adds JTextField
             if (masks[row][col]) {
-               tfCells[row][col].setText("");            // set to empty string
-               tfCells[row][col].setEditable(true);
-               tfCells[row][col].setBackground(OPEN_CELL_BGCOLOR);
+               boardCells[row][col].setText("");            // set to empty string
+               boardCells[row][col].setEditable(true);
+               boardCells[row][col].setBackground(INPUT_CELL_COLOR);
  
                // Add ActionEvent listener to process the input
                // ... [TODO 4] (Later) ...
             } 
             else 
             {
-               tfCells[row][col].setText(puzzle[row][col] + "");
-               tfCells[row][col].setEditable(false);
-               tfCells[row][col].setBackground(CLOSED_CELL_BGCOLOR);
-               tfCells[row][col].setForeground(CLOSED_CELL_TEXT);
+               boardCells[row][col].setText(solution[row][col] + "");
+               boardCells[row][col].setEditable(false);
+               boardCells[row][col].setBackground(FIXED_CELL_COLOR);
+               boardCells[row][col].setForeground(FIXED_CELL_TEXT);
             }
             // Beautify all the cells
-            tfCells[row][col].setHorizontalAlignment(JTextField.CENTER);
-            tfCells[row][col].setFont(FONT_NUMBERS);
+            boardCells[row][col].setHorizontalAlignment(JTextField.CENTER);
+            boardCells[row][col].setFont(FONT_NUMBERS);
          }
       }
  
@@ -161,24 +172,24 @@ public class Sudoku extends javax.swing.JFrame
    {
       
       
-      for (int row = 0; row < GRID_SIZE; row ++) 
+      for (int row = 0; row < BOARD_SIZE; row ++) 
       {
-         for(int col = 0; col < GRID_SIZE; col ++)
+         for(int col = 0; col < BOARD_SIZE; col ++)
          {
              if(masks[row][col] == false)
              {
-                 tempArray[row][col] = puzzle[row][col];
+                 tempArray[row][col] = solution[row][col];
                 
              }
              else
              {
-                if(tfCells[row][col].getText().equals(""))
+                if(boardCells[row][col].getText().equals(""))
                 {
                    tempArray[row][col] = -1;
                 }
                 else
                 {
-                   tempArray[row][col] = Integer.valueOf(tfCells[row][col].getText().toString()); 
+                   tempArray[row][col] = Integer.valueOf(boardCells[row][col].getText().toString()); 
                 }
              }
          }
@@ -189,11 +200,11 @@ public class Sudoku extends javax.swing.JFrame
    {
        initArray();
        boolean same = true;
-       for(int row = 0; row < GRID_SIZE; row ++)
+       for(int row = 0; row < BOARD_SIZE; row ++)
        {
-           for(int col = 0; col < GRID_SIZE; col ++)
+           for(int col = 0; col < BOARD_SIZE; col ++)
            {
-               if(tempArray[row][col] != puzzle[row][col])
+               if(tempArray[row][col] != solution[row][col])
                {
                    same = false;
                    totalScore -= 10;
